7505bdd6cb2c42859c2cc33ddabe3ca6ed5f4201
rooneyk <rooneyk@colorado.edu>
2014-09-17 16:08:16 -0600

    Update HelloWorld.java
    This is a comment

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Displays the string.
-		System.out.println("How Are You?");
+		System.out.println("How Are You?"); // Displays the string.
 	
 
 }
